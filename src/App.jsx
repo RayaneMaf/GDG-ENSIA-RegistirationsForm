de165c468fc949fb69280314859381acd5155917
Rayane <maaroufrayane3@gmail.com>
2025-09-29 22:19:54 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
-import MyPersistentInput from "./components/DepartmentRankTable/DepartmentRankTable";
 import DepartmentRankTable from "./components/DepartmentRankTable/DepartmentRankTable";
-import PersonalInfos from "./components/PersonalInfos/PersonalInfos";
 
 function App() {
   return (
     <>
       {/* <PersonalInfos /> */}
       <DepartmentRankTable />
-      <MyPersistentInput />
     </>
   );
 }
